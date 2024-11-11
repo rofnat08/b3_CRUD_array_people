fb880587c594dc9eb340459ed7fd9257b914ecb5
rofnat08 <rofna08@gmail.com>
2024-11-10 19:14:16 -0500

    Methods edit and create People

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -4,6 +4,10 @@ import { useState } from "react";
 export const People = ({people, setPeople}) => {
   // Estado para gestionar el ID de la persona que se està editando
   const [editingId, setEditingId] = useState(null);
+
+  // Variable de estado para saber si la persona està editando o no
+  const [isEditing, setIsEditing] = useState(false); //Para dar el nombre de una variable booleana es con is al inicio para react jsx
+
   //Estado para almacenar temporalmente esos datos de la persona que se està editando (arreglo people)
   const [editedPerson, setEditedPerson] = useState(
     //solo se edita name role e img
@@ -31,8 +35,16 @@ export const People = ({people, setPeople}) => {
     setPeople([...people, {id: people.length + 1, ...editedPerson}]);
     // reiniciar el estado del formulario
     setEditedPerson({name: '', role: '', img: ''}) //Se limpian los campos del formulario
-
   };
+
+  //Metodo para editar a una persona (Se estable el id de la persona que se va a editar)
+  const handleEdit = (id) => {
+    setEditingId(id);
+    setIsEditing(true); //cuando la persona hace click en editar se cambia el estado de la variable que se llama setIsEditing a true 
+    const personToEdit = people.find(person => person.id === id); //coincidencia exacta mismo valor y tipo de dato
+    
+    setEditedPerson({...personToEdit});
+};
 
   return (
     <div>
@@ -48,6 +60,7 @@ export const People = ({people, setPeople}) => {
                                     name={people.name}
                                     img={people.img}
                                     role={people.role}
+                                    handleEdit={() => handleEdit(people.id)}
                                 />
                             </div>
                         );
@@ -56,13 +69,26 @@ export const People = ({people, setPeople}) => {
             </div>
         </div>
         {/* Se crea el formulario  */}
-        <div className="container mt-4 row p-2">
-            <h2 className="text-center">Crear Nuevo Empleado</h2>
+        <div className="container">
+            <h2 className="text-center mt-4">Crear Nuevo Empleado</h2>
             <form action="">
                 <div>
                     <label htmlFor="name">Nombres</label>
                     {/* input por convencion debe tener un name y opcional un valor por defecto  */}
-                    <input type="text" name="name" value={editedPerson.name} required className="form-control"/>
+                    <input type="text" name="name" value={editedPerson.name} onChange={handleChange} required className="form-control"/>
+                </div>
+                <div>
+                    <label htmlFor="role">Rol</label>
+                    {/* input por convencion debe tener un name y opcional un valor por defecto  */}
+                    <input type="text" name="role" value={editedPerson.role} onChange={handleChange} required className="form-control"/>
+                </div>
+                <div>
+                    <label htmlFor="img">Avatar</label>
+                    {/* input por convencion debe tener un name y opcional un valor por defecto  */}
+                    <input type="text" name="img" value={editedPerson.img} onChange={handleChange} required className="form-control"/>
+                </div>
+                <div className="mt-2 text-center">
+                    <button type="submit" className="btn btn-primary">Modificar</button>
                 </div>
             </form>
         </div>
